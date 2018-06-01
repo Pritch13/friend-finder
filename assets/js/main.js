58c002266f08de81d1380478d64e3166db5760e6
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-01 13:31:48 -0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#intro', {
+    strings: ["Find Friends...","Find Partners...","Find Bro's...","Find Mates...","Find Love...","Find Family..." ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    smartBackspace: true // Default value
+});
+
+// var typed = new Typed("#intro", {
+//     strings: ["This is a JavaScript library", "This is an ES6 module"],
+//     smartBackspace: true // Default value
+//   });
