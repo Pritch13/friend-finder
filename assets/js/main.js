d092c68eb081288d4dc9e979c316bbb1e7493c13
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-02 19:36:04 -0600

    working scale

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,61 +12,59 @@ $(".container").hide();
 $(".container").fadeIn(3000);
 
 $("#exploreSurv").on("click", function (event) {
-    event.preventDefault();
 
-    var scoreArr = [
-        $("#answer1").val(), $("#answer2").val(), $("#answer3").val(), $("#answer4").val(), $("#answer5").val(),
-        $("#answer6").val(), $("#answer7").val(), $("#answer8").val(), $("#answer9").val(), $("#answer10").val()
-    ];
+    if ($("#friendName").val() === "" || $("#friendUrl").val() === "") {
+        alert('Please complete all fields!')
+    } else {
+        event.preventDefault();
 
-    var newFriend = {
-        name: $("#friendName").val().trim(),
-        photo: $("#friendUrl").val().trim(),
-        score: scoreArr
-    };
-
-
-    $(".surveyCont").fadeOut();
-    setTimeout(function () {
-        $("#destText").fadeIn("slow");
-    }, 1200);
-    setInterval(function () {
-        $("#destText").fadeOut("slow");
+        var scoreArr = [
+            $("#answer1").val(), $("#answer2").val(), $("#answer3").val(), $("#answer4").val(), $("#answer5").val(),
+            $("#answer6").val(), $("#answer7").val(), $("#answer8").val(), $("#answer9").val(), $("#answer10").val()
+        ];
+    
+        var newFriend = {
+            name: $("#friendName").val().trim(),
+            photo: $("#friendUrl").val().trim(),
+            score: scoreArr
+        };
+    
+        $(".surveyCont").fadeOut();
         setTimeout(function () {
-            $(".matchInfo").fadeIn("slow");
-            $(".again").fadeIn("slow");
-        }, 1000);
-    }, 3000);
-
-    var currentURL = window.location.origin;
-    // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
-    $.ajax({ url: currentURL + "/api/friends", method: "GET" })
-        .then(function (friendsData) {
- 
-
-
-            
-        });
-
-    $.post("/api/friends", newFriend,
-        function (data) {
-
-            $("#friendName").val("");
-            $("#friendUrl").val("");
-            $("#answer1").val("1");
-            $("#answer2").val("1");
-            $("#answer3").val("1");
-            $("#answer4").val("1");
-            $("#answer5").val("1");
-            $("#answer6").val("1");
-            $("#answer7").val("1");
-            $("#answer8").val("1");
-            $("#answer9").val("1");
-            $("#answer10").val("1");
-
-            $("#matchName").text(data.name);
-            $(".matchInfo").append("<img class='matchPic'src=" + data.pic + " alt='matchedPerson'>")
-
-
-        });
+            $("#destText").fadeIn("slow");
+        }, 1200);
+        setInterval(function () {
+            $("#destText").fadeOut("slow");
+            setTimeout(function () {
+                $(".matchInfo").fadeIn("slow");
+                $(".again").fadeIn("slow");
+            }, 1000);
+        }, 3000);
+    
+        // var currentURL = window.location.origin;
+        // // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+        // // $.ajax({ url: currentURL + "/api/friends", method: "GET" })
+        // //     .then(function (friendsData) {   
+        // //     });
+    
+        $.post("/api/friends", newFriend,
+            function (data) {
+    
+                $("#friendName").val("");
+                $("#friendUrl").val("");
+                $("#answer1").val("1");
+                $("#answer2").val("1");
+                $("#answer3").val("1");
+                $("#answer4").val("1");
+                $("#answer5").val("1");
+                $("#answer6").val("1");
+                $("#answer7").val("1");
+                $("#answer8").val("1");
+                $("#answer9").val("1");
+                $("#answer10").val("1");
+    
+                $("#matchName").text(data.name);
+                $(".matchInfo").append("<img class='matchPic'src=" + data.pic + " alt='matchedPerson'>")
+            });
+    }
 });
